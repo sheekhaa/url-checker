d691b226308773e09ca09ced64b775d238181039
Shikha Kandel <shikhakandel65@gmail.com>
2026-05-11 13:10:21 +0545

    improve accuracy

--- src/services/bazosServices.ts
+++ src/services/bazosServices.ts
@@ -1,10 +1,7 @@
 export const handleBazos = async (url: string, body: string) => {
   const text = body.toLowerCase();
 
-  const isDead =
-    text.includes("inzerát byl smazán") ||
-    text.includes("inzerát nenalezen") ||
-    text.includes("this ad is no longer available");
+  const isDead = text.includes("this ad is no longer available");
 
   if (isDead) {
     return { url, status: 404, message: "Not Found " };
